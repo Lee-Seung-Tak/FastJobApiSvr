27d21f91939e0d569d8bdeefdc95b41fefc32db4
lst <lstlove9804@gmail.com>
2025-04-23 16:50:31 +0900

    lst add / login시 사용하는 DTO 초기 구조 로직 추가

--- auth/controller/auth.controller.js
+++ auth/controller/auth.controller.js
@@ -3,13 +3,10 @@ const LoginDTO   = require('../dto/auth.dto')
 
 
 exports.login = async ( req, res ) =>{
-    //1. cluster 사용 
-    //2. 2개의 코어 사용
     try{
         const { email, password } = req.body;
-        const loginData           = new LoginDTO( email, password );
-        LoginDTO.isValid( loginData );
 
+        const loginDTO = LoginDTO.isValid({ email, password });
         AuthServer.login()
     }catch(error){
         if (error.message === 'Email and password are required')
