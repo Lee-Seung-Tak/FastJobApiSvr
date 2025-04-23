2078947d26cf916745bde67912a0f9be14a6962d
lst <lstlove9804@gmail.com>
2025-04-23 16:51:38 +0900

    lst add / db pool 선언 및 query.json 선언

---
+++ database/db.js
@@ -0,0 +1,19 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+import pool from '../database/db'
+dotenv.config(); // 가장 먼저 호출해야 함
+
+const { Pool } = pg;
+
+const pool = new Pool({
+    host:     process.env.DB_HOST,
+    user:     process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT), 
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  });
+  
+  export default pool;
