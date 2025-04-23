ace0f35e1ab5d74036c6011ea08e60c1929c610d
lst <lstlove9804@gmail.com>
2025-04-23 23:04:41 +0900

    lst add / login 시 error message 에러에 맞춰 http status code return 로직 추가

--- auth/controller/auth.controller.js
+++ auth/controller/auth.controller.js
@@ -1,15 +1,15 @@
-const AuthServer = require('../service/auth.service');
+const authService = require('@auth_service');
 const LoginDTO   = require('../dto/auth.dto')
 
 
-exports.login = async ( req, res ) =>{
-    try{
+exports.login = async ( req, res ) => {
+    try {
         const { email, password } = req.body;
 
-        const loginDTO = LoginDTO.isValid({ email, password });
-        AuthServer.login()
-    }catch(error){
+        const loginDTO = LoginDTO.isValid( { email, password } );
+        authService.login()
+    } catch( error ) {
         if (error.message === 'Email and password are required')
-            return res.status(400).json({ message : error.message });
+            return res.status(400).json( { message : error.message } );
     }
 }
