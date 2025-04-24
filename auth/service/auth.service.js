e420af05ab732d8ac5ba908b1aa6cc5e50a65e45
lst <lstlove9804@gmail.com>
2025-04-24 11:14:52 +0900

    lst add / service에서 db query가 동작할 수 있도록 수정 및 모듈변수 이름 수정

--- auth/service/auth.service.js
+++ auth/service/auth.service.js
@@ -1,8 +1,10 @@
 
-const pool    = require('@db')
-const query   = require('@query')
+const db        = require('@db')
+const queryJson = require('@query')
+
 
 exports.login = async( email, password ) => {
-    
-    const result = await pool.query( query.login, [email]);
+    const result = await db.query('SELECT NOW()');
+    console.log(result);
+    //const result = await pool.query( query.login, [email]);
 }
