9d2f327fedf11e709e21a9c28b8e155535948baa
lst <lstlove9804@gmail.com>
2025-04-29 00:30:17 +0900

    lst add / 미들 웨어 중복되는 로직 삭제 후 app.js 에 추가

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 require('module-alias/register');
 
-const express = require('express');
-const app     = express();
-const port    = 4000
+const express    = require('express');
+const app        = express();
+const port       = 4000
 
 const middleWare = require('@middleware')
-
-const cluster = require('cluster');
-const os      = require('os');
-const CPU     = 2;
+const cluster    = require('cluster');
+const os         = require('os');
+const CPU        = 2;
 
 
 const authRouter = require('@auth_router')
 
 app.use( express.json() );
+const multer = require('multer');
+
 app.use( '/auth', authRouter );
-//app.use( middleWare.verifyToken );
 app.use( ( req, res, next ) => {
-    if (req.path === '/auth/login' ) return next();
+    if ( req.path === '/auth/login' || req.path === '/auth/signup') return next();
     middleWare.verifyToken( req, res, next );
 })
-if (cluster.isMaster){
+
+if (cluster.isMaster) {
     for (let i = 0; i< CPU; i++)
         cluster.fork();
 } else {
