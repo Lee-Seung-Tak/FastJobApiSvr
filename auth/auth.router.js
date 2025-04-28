fbe39412737c486b7fc31beec42abae21cb15eab
lst <lstlove9804@gmail.com>
2025-04-28 19:18:32 +0900

    lst add / 회원가입 라우터 추가

--- auth/auth.router.js
+++ auth/auth.router.js
@@ -6,4 +6,5 @@ const middleWare     = require('@middleware')
 // EndPoint
 // router.post('/login', middleWare.verifyToken, authController.login);
 router.post('/login',  authController.login);
+router.post('/signup', authController.signUp);
 module.exports = router;
