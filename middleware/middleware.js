e7c8df32e07e260066d8a68ba884aaa50c5ce5f6
lst <lstlove9804@gmail.com>
2025-04-29 00:32:51 +0900

    lst add / middleware의 중복되는 부분 app.js로 이동

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,10 +1,6 @@
 
 exports.verifyToken = ( req, res, next ) => {
-    
-    if( req.path === '/auth/login' || req.path === '/auth/signup')
-    {
-        return next();
-    }
+
     console.log("[LOG] - middleware here");
     next();
 }
