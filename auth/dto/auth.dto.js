83bd44fb69420d22b26c29750e224b8bfd903c68
lst <lstlove9804@gmail.com>
2025-04-28 17:51:27 +0900

    lst add / login시 데이터 검증하는 중복 로직 최적화

--- auth/dto/auth.dto.js
+++ auth/dto/auth.dto.js
@@ -1,24 +1,26 @@
-class LoginDTO{
+class LoginDTO { 
     // 객체의 email, password 초기화
     constructor( userId, password ) {
         this.userId   = userId;
         this.password = password;
     }
 
-    static isValid( loginData ) {
-        try {
-
-            const { userId, password } = loginData;
-            if (!userId || !password)
-                throw new Error('user_id and password are required');
-            
-            return new LoginDTO( userId, password );
-            
-        } catch ( error ) {
+    static isValid(loginData) {
+        // 조건 수정 -> loginData가 없거나, loginData.userId 및 password가 없거나
+        // 중복되는 조건 최적화
+        if (!loginData || !loginData.userId || !loginData.password) {
             throw new Error('user_id and password are required');
         }
+        const { userId, password } = loginData;
+        return new LoginDTO(userId, password);
     }
 
 }
 
-module.exports = LoginDTO;
+class SignUpDTO {
+
+}
+module.exports = {
+    SignUpDTO,
+    LoginDTO
+}
