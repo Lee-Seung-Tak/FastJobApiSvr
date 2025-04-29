7e5f9f60ff04362eeac880527e51a799672d9ce6
lst <lstlove9804@gmail.com>
2025-04-29 23:47:37 +0900

    lst add / 사용자가 전송한 파일들 저장하는 미들웨어 추가

--- auth/auth.router.js
+++ auth/auth.router.js
@@ -5,7 +5,19 @@ const authController = require('@auth_controller');
 
 
 const multer         = require('multer');
-const userData       = multer( { dest: 'uploads/'} );
+const path           = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // 저장 폴더
+  },
+  filename: function (req, file, cb) {
+    const originalName = `${req.body.user_id}_${file.fieldname}_${file.originalname}`;
+    cb( null, originalName );
+  }
+});
+
+const userData = multer({ storage: storage });
 
 router.post('/login',  authController.login);
 router.post('/signup', userData.any(), authController.signUp);
