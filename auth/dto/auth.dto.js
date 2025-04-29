700d932b6a795908512cd6c0b9c274c1dbfa97db
lst <lstlove9804@gmail.com>
2025-04-30 00:28:58 +0900

    lst add /signup data 유효성 검사 추가

--- auth/dto/auth.dto.js
+++ auth/dto/auth.dto.js
@@ -5,7 +5,7 @@ class LoginDTO {
         this.password = password;
     }
 
-    static isValid(loginData) {
+    static isValid ( loginData ) {
         // 조건 수정 -> loginData가 없거나, loginData.userId 및 password가 없거나
         // 중복되는 조건 최적화
         if (!loginData || !loginData.userId || !loginData.password) {
@@ -18,6 +18,31 @@ class LoginDTO {
 }
 
 class SignUpDTO {
+    constructor ( userData ) {
+        //name, email, phone, user_id, password, category, 
+        // resume, resume_url, self_intro, self_intro_url, carrer_desc, career_desc_url, portpolio_url
+        this.name     = userData.name;
+        this.email    = userData.email;
+        this.phone    = userData.phone;
+        this.userId   = userData.userId;
+        this.password = userData.password;
+        this.category = userData.category;
+    }
+    static isValid(userData) {
+        if (
+          !userData ||
+          !userData.name     ||
+          !userData.email    ||
+          !userData.phone    ||
+          !userData.userId   ||
+          !userData.password ||
+          !userData.category
+        ) {
+          throw new Error('check uer data(name, email, phone, userId, password, category)');
+        }
+      
+        return new SignUpDTO(userData);
+      }
 
 }
 module.exports = {
