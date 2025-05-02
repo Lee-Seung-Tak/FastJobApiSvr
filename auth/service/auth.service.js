3a6208be173950c19eaaad3f70f6ea9eb5b7ade3
lst <lstlove9804@gmail.com>
2025-05-02 17:47:23 +0900

    lst add / 메일 전송 로직 추가

--- auth/service/auth.service.js
+++ auth/service/auth.service.js
@@ -4,6 +4,9 @@ const queryJson               = require('@query');
 const jsonwebtoken            = require('jsonwebtoken');
 const { PG_UNIQUE_VIOLATION } = require('postgres-error-codes');
 const dotenv                  = require('dotenv')
+const fs                      = require('fs');
+const path                    = require('path');
+const nodemailer              = require('nodemailer');
 dotenv.config();
 
 
@@ -44,17 +47,61 @@ exports.login = async( userId, password ) => {
     }
 }
 
+
+const makeSignUpToken = async ( email ) => {
+    return jwt.sign( { email : email }, process.env.REFRESH_SECRET, { expiresIn: '5m' } );
+}
+// 이메일 발송 함수
+const sendMailForSignUp = async ( email ) => {
+
+    const signUpToken = makeSignUpToken(email)
+    const filePath    = path.join(__dirname, 'index.html');
+    const mailBody    = fs.readFileSync(filePath, 'utf8');
+    mailBody          = mailBody.replace('{TOKEN}',signUpToken);
+
+    const transPorter = nodemailer.createTransport({
+        service:"gmail",
+        host: "smtp.gmail.email",
+        port: 587,
+        secure: false, 
+        auth: {
+          user: process.env.SYS_EMAIL,
+          pass: process.env.SYS_EMAIL_KEY,
+        },
+      });
+
+    const mailOptions = {
+        from   : process.env.SYS_EMAIL,  
+        to     : email,                         
+        subject: '[회원가입 인증 메일]',                       
+        html   : mailBody                           
+    };
+
+    try {
+        const info = await transPorter.sendMail(mailOptions);
+        console.log('Email sent: ' + info.response);
+        return info.response;  // 이메일 발송 성공 메시지 반환
+
+    } catch (error) {
+        console.error('Error sending email:', error);
+        throw new Error('Failed to send email');  // 이메일 발송 실패 시 에러 발생
+    }
+};
 exports.signUp = async ( userData, userFiles ) => {
     try {
-        console.log(userFiles)
         let queryResult = await db.query( queryJson.checkIdDuplicate, [ userData.userId ] );
-        queryResult     = queryResult.rows[0];
-        if (queryResult) throw new Error('user is duplicate');
-        console.log('userData : ', userData, 'userFiles:', userFiles)
-        //if
+        queryResult     = queryResult.rows;
+        if ( queryResult == [] ) throw new Error('user is duplicate');
+        await sendMailForSignUp(userData.email)
+       
         
     } catch (error) {
         throw new Error(error)
 
     }
+}
+
+
+exports.signUpVerify = async() => {
+
 }
