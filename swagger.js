1b04a3d5aac2e583e8ada80237f7540d9b4e079d
lst <lstlove9804@gmail.com>
2025-05-02 12:19:53 +0900

    lst add / api docs를 위한 swagger.js추가

---
+++ swagger.js
@@ -0,0 +1,16 @@
+// swagger.js
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'FastJobAPI Docs',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./auth/*.js', './routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+module.exports = swaggerSpec;
