9b46095306d2432c762ac80d30bf935004346ccc
lst <lstlove9804@gmail.com>
2025-05-08 02:36:09 +0900

    lst add / 줄맞춤 + password 유무 및, 신규 비밀번호와 컨펌 비밀번호 비교 로직 위치 수정

--- users/service/users.service.js
+++ users/service/users.service.js
@@ -7,27 +7,35 @@ require('dotenv').config();
 
 // 통합 유저 정보 수정 (비밀번호·전화·주소)
 exports.patchUser = async ( patchUserData ) => {
-    const { userId, oldPassword, newPassword, confirmPassword, phone } = patchUserData;
-    // 비밀번호 변경
-     // 연락처
-      if (phone) {
-        const result = await db.query(queryJson.updatePhone, [phone, userId]);
-        console.log('updatePhone rowCount:', result.rowCount);
-      }
-  
-      if (oldPassword || newPassword || confirmPassword) {
-       if (!newPassword || newPassword !== confirmPassword) {
-        const err = new Error('New passwords do not match'); err.statusCode = 400; 
-        throw err;
-      }
-      let queryResult = await db.query(queryJson.login, [userId]);
-      queryResult     = queryResult.rows[0];
-       if (!queryResult || queryResult.password !== oldPassword) {
-        const err = new Error('Old password is incorrect'); err.statusCode = 401; 
-        throw err;
-      }
-      await db.query(queryJson.updatePassword, [newPassword, userId]);
+
+  const { userId, oldPassword, newPassword, confirmPassword, phone } = patchUserData;
+
+  // lst add / password 유무 및, newPassword와 confirmPassword 조건에 따른 에러 리턴이면
+  // 전개 연산 이후에 바로 진행하는게 효율적
+  if ( !newPassword || newPassword !== confirmPassword ) {
+    const err = new Error('New passwords do not match'); err.statusCode = 400; 
+    throw err;
+  }
+
+  if ( phone ) {
+    const result = await db.query( queryJson.patchPhoneNumber, [phone, userId] );
+    console.log('updatePhone rowCount:', result.rowCount);
+  }
+
+  if ( oldPassword || newPassword || confirmPassword ) {
+
+    // lst add / login -> getUserPassword라는 신규 쿼리 추가
+    let queryResult = await db.query( queryJson.getUserPassword, [userId] );
+    queryResult     = queryResult.rows[0].password;
+    
+    if (!queryResult || queryResult !== oldPassword) {
+      const err = new Error('Old password is incorrect'); err.statusCode = 401; 
+      throw err;
     }
-    return { message: 'User information updated successfully' };
-  };
+    
+    await db.query( queryJson.patchPassword, [newPassword, userId] );
+  }
+
+  return { message: 'User information updated successfully' };
+};
   
