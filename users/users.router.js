71c741501820cef49832a3eaef79b1a2a44020f5
lst <lstlove9804@gmail.com>
2025-05-08 02:35:09 +0900

    lst add / 라우터 위치 변경 및 과도한 공백 삭제

--- users/users.router.js
+++ users/users.router.js
@@ -1,8 +1,9 @@
 const express            = require('express');
-const router             = express.Router();
 const usersController    = require('@users_controller');
 const multer             = require('multer');
 const path               = require('path');
 
-router.patch('/user',      usersController.patchUser);
+const router             = express.Router();
+
+router.patch('/user', usersController.patchUser);
 module.exports = router;
