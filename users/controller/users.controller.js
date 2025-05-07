52a2de5a47cddfb080ef664890a62e397104588d
lst <lstlove9804@gmail.com>
2025-05-08 02:35:33 +0900

    lst add / 코드 블럭 내 줄맞춤이 하나도 되어 있지 않아서 줄 맞춤

--- users/controller/users.controller.js
+++ users/controller/users.controller.js
@@ -2,11 +2,11 @@ const usersService = require('@users_service');
 const authDTO      = require('@auth_dto');
 
 exports.patchUser = async (req, res) => {
-    try { console.log(req.body);
-      const result = await usersService.patchUser(req.body);
-     
-      return res.status(200).json(result);
-    } catch (err) {
-      return res.status(err.statusCode || 400).json({ message: err.message });
-    }
-  };
+
+  try { 
+    const result = await usersService.patchUser(req.body);
+    return res.status(200).json(result);
+  } catch (err) {
+    return res.status(err.statusCode || 400).json({ message: err.message });
+  }
+};
