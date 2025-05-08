6f52771c9cdc90302d343d5b547db2e384710d5f
lst <lstlove9804@gmail.com>
2025-05-08 23:38:11 +0900

    lst add / 사용자의 Bearer Token 별 userId 추출 하여 req에 추가하는 로직 추가

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,6 +1,24 @@
+const dotenv = require('dotenv');
+const jwt    = require('jsonwebtoken');
+dotenv.config();
 
-exports.verifyToken = ( req, res, next ) => {
+exports.verifyToken = (req, res, next) => {
+    const authHeader = req.get('Authorization');
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Authorization header missing' });
+    }
 
-    console.log("[LOG] - middleware here");
-    next();
-}
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Token not found' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+        req.userId = decoded.userId; // 디코딩된 사용자 정보 저장
+        next();
+    } catch (err) {
+        return res.status(403).json({ message: 'Invalid or expired token' });
+    }
+};
