6b5e2ed78dae484309905e9dc514f5b478df9a72
ltj <xowjd8465@gmail.com>
2025-05-09 19:46:36 +0900

    ltj add / userId를 토큰에서 가져오는걸로 수정 및 getUser 비지니스 로직 추가

--- users/service/users.service.js
+++ users/service/users.service.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 // 통합 유저 정보 수정 (비밀번호·전화·주소)
 exports.patchUser = async ( patchUserData ) => {
 
-  const { userId, oldPassword, newPassword, confirmPassword, phone } = patchUserData;
+  const { oldPassword, newPassword, confirmPassword, phone } = patchUserData.body;
 
   // lst add / password 유무 및, newPassword와 confirmPassword 조건에 따른 에러 리턴이면
   // 전개 연산 이후에 바로 진행하는게 효율적
@@ -18,14 +18,14 @@ exports.patchUser = async ( patchUserData ) => {
   }
 
   if ( phone ) {
-    const result = await db.query( queryJson.patchPhoneNumber, [phone, userId] );
+    const result = await db.query( queryJson.patchPhoneNumber, [phone, patchUserData.userId] );
     console.log('updatePhone rowCount:', result.rowCount);
   }
 
   if ( oldPassword || newPassword || confirmPassword ) {
 
     // lst add / login -> getUserPassword라는 신규 쿼리 추가
-    let queryResult = await db.query( queryJson.getUserPassword, [userId] );
+    let queryResult = await db.query( queryJson.getUserPassword, [patchUserData.userId] );
     queryResult     = queryResult.rows[0].password;
     
     if (!queryResult || queryResult !== oldPassword) {
@@ -33,9 +33,34 @@ exports.patchUser = async ( patchUserData ) => {
       throw err;
     }
     
-    await db.query( queryJson.patchPassword, [newPassword, userId] );
+    await db.query( queryJson.patchPassword, [newPassword, patchUserData.userId] );
   }
 
   return { message: 'User information updated successfully' };
 };
+
+exports.getUser = async ( userId ) => {
   
+  if ( !userId ) {
+    const err = new Error('user Id is required');
+    err.statusCode = 400;
+    throw err;
+  }
+    // DB에서 유저 조회회
+    let queryResult = await db.query( queryJson.getUserById, [userId] );
+    queryResult = queryResult.row[0];
+
+    if (!user) {
+      const err = new Error('User not found');
+      err.statusCode = 404;
+      throw err;
+    }
+    // 비밀번호 정보는 제외하고 반환환
+    const { password, ...userInfo } = user;
+    return userInfo;
+};
+
+exports.getUserInfo = async( userId ) => {
+  const { rows } = await db.query( queryJson.getUserData, [userId] );
+  return rows.length ? rows[0] : null;
+};
