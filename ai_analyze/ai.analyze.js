2f5bb8d49996829cac6f4dcac2d8bf1c910f7f13
lst <lstlove9804@gmail.com>
2025-05-12 02:29:31 +0900

    lst add / use ai model function add

---
+++ ai_analyze/ai.analyze.js
@@ -0,0 +1,39 @@
+const fs     = require('fs');
+const pdf    = require('pdf-parse');
+const gemini = require('./gemini.js'); 
+const db     = require('@db');
+const query  = require('@query');
+
+exports.aiAnalyzeResume = async ( userId, fileUrl ) => {
+    const ResumeUrl      = `./${fileUrl}`;
+    const dataBuffer     = fs.readFileSync( ResumeUrl );
+    let pdfData          = await pdf(dataBuffer);
+    pdfData              = pdfData.text.replace(/\n/g, '');
+    const aiAnlaysReulst = await gemini.generateContent( pdfData );
+
+    await db.query(query.updateResume, [ aiAnlaysReulst, userId ] )
+}
+
+exports.aiAnalyzeSelfIntro = async ( userId, fileUrl ) => {
+    const ResumeUrl      = `./${fileUrl}`;
+    const dataBuffer     = fs.readFileSync( ResumeUrl );
+    let pdfData          = await pdf(dataBuffer);
+    pdfData              = pdfData.text.replace(/\n/g, '');
+    const aiAnlaysReulst = await gemini.generateContent( pdfData );
+
+    await db.query(query.updateSelfIntro, [ aiAnlaysReulst, userId ] )
+}
+
+
+exports.aiAnalyzeCarrerDesc = async ( userId, fileUrl ) => {
+    const ResumeUrl      = `./${fileUrl}`;
+    const dataBuffer     = fs.readFileSync( ResumeUrl );
+    let pdfData          = await pdf(dataBuffer);
+    pdfData              = pdfData.text.replace(/\n/g, '');
+    const aiAnlaysReulst = await gemini.generateContent( pdfData );
+
+    await db.query(query.updateCarrerDesc, [ aiAnlaysReulst, userId ] )
+}
+
+
+
---
+++ ai_analyze/gemini.js
@@ -0,0 +1,15 @@
+const { GoogleGenAI } = require('@google/genai'); 
+const ai = new GoogleGenAI({ apiKey: "AIzaSyAWuH-6ucMg35U5ZdI_ukCV6m7UKldCP-Y" });
+
+//import { GoogleGenAI } from '@google/genai';
+exports.generateContent = async(data) =>{
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: `제가 제공한 데이터는 PDF에서 추출한 내용입니다. 이 내용에서 중요한 정보들을 추려내어 기승전결 구조에 맞춰서, 
+        각각의 부분이 자연스럽게 연결되도록 정리해주세요. 예를 들어, 처음에는 주제를 간략히 소개하고, 
+        그 후에는 내용을 확장하여 설명한 뒤, 마지막에는 결론을 내려주세요.
+        PDF에서 추출한 데이터 : ${data}`,
+      });
+      return response.text
+}
+
