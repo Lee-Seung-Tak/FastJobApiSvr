e764c435bd8bc1d1f7c2f4b2485bdd9b33644654
ltj <xowjd8465@gmail.com>
2025-05-12 09:29:09 +0900

    ltj add / patchResumeUrl에 디버깅용 req.file 로그 추가

--- users/controller/users.controller.js
+++ users/controller/users.controller.js
@@ -1,6 +1,7 @@
 const usersService = require('@users_service');
 const authDTO      = require('@auth_dto');
-
+const path         = require('path');
+const multer       = require('multer');
 exports.patchUser = async ( req, res ) => {
 
   try { 
@@ -20,5 +21,20 @@ exports.getUser = async ( req, res ) => {
   } catch ( err ) {
       console.error(err);
       return res.status(400).json({ message: 'Bad request' });
+  }
+};
+
+exports.patchResumeUrl = async (req, res) => {
+  console.log('> req.file =', req.file);
+  try {
+    const uploadDir = path.resolve(__dirname, '../../uploads');
+    const result = await usersService.patchResumeUrl({
+      userId: req.userId,   // 토큰 미들웨어가 주입한 사용자 ID
+      file:   req.file,     // multer.single이 파싱한 resume 파일
+      uploadDir
+    });
+    return res.status(200).json({ data: result });
+  } catch (err) {
+    return res.status(err.statusCode||500).json({ message: err.message });
   }
 };
