b207876e49023411b8fac82c7881721709b89913
lst <lstlove9804@gmail.com>
2025-05-14 21:51:13 +0900

    lst add / token refresh에 대한 스웨거 추가

--- auth/auth.router.js
+++ auth/auth.router.js
@@ -188,8 +188,80 @@ router.post('/login',  authController.login);
  */
 router.post('/signup', userData.any(), authController.signUp);
 
-
+/**
+ * @swagger
+ * /signup-verify:
+ *   get:
+ *     summary: "회원가입 검증"
+ *     description: "Bearer 토큰을 사용하여 회원가입을 검증합니다. 토큰이 정상적인 경우 400을, 비정상적인 경우 401을 반환합니다."
+ *     tags:
+ *       - "Auth"
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: "정상 토큰으로 확인됨."
+ *       400:
+ *         description: "이미 가입된 사용자입니다."
+ *       401:
+ *         description: "유효하지 않은 토큰입니다."
+ *       500:
+ *         description: "서버 오류."
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         required: true
+ *         description: "Bearer 토큰"
+ *         schema:
+ *           type: string
+ *           example: "Bearer your_token_here"
+ */
 router.get('/signup-verify', authController.signUpVerify);
+
+
+/**
+ * @swagger
+ * /auth/token-refresh:
+ *   post:
+ *     summary: Access Token 갱신
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - refresh_token
+ *             properties:
+ *               refresh_token:
+ *                 type: string
+ *                 description: 갱신 토큰
+ *                 example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ0ZXN0MSIsImlhdCI6MTc0NzE5MTYzNCwiZXhwIjoxNzQ3Nzk2NDM0fQ.9jagB1k6Y_tclrZfiJf4x1yUnc5lYzNoMI6oOM8gzZ8"
+ *     responses:
+ *       200:
+ *         description: Access Token 갱신 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 tokens:
+ *                   type: object
+ *                   properties:
+ *                     access_token:
+ *                       type: string
+ *                       description: 새로 발급된 Access Token
+ *                       example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ0ZXN0MSIsImlhdCI6MTc0NzE5MTY4MiwiZXhwIjoxNzQ3MTk1MjgyfQ.lOziyrvmI5JWbxFxNyV6H5LJXoCpfilKR6Fl7ziXWbk"
+ *                     refresh_token:
+ *                       type: string
+ *                       description: 새로 발급된 Refresh Token 
+ *                       example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ0ZXN0MSIsImlhdCI6MTc0NzE5MTY4MiwiZXhwIjoxNzQ3Nzk2NDgyfQ.JhaVbmBGUGoVo0j-Ce-l47ti0oZV9vLfa6ixZ9tx-t4"
+ *       401:
+ *         description: 유효하지 않거나 만료된 Refresh Token
+ *       500:
+ *         description: 서버 오류
+ */
 
 router.post('/token-refresh', authController.tokenRefresh);
 module.exports = router;
