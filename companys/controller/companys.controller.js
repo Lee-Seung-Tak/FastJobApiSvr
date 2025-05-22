a760843772b717673368f1c71aa4b91328ce674c
ltj <xowjd8465@gmail.com>
2025-05-22 09:31:29 +0900

    ltj add / signup 컨트롤러 구현

---
+++ companys/controller/companys.controller.js
@@ -0,0 +1,21 @@
+const companysService = require('@companys_service');
+const companysDTO     = require('@companys_dto');
+const path            = require('path');
+
+
+const signUpDTO   = companysDTO.SignUpDTO;
+
+exports.signUp = async ( req, res ) => {
+    try {
+        console.log(req.body)
+        const userData = signUpDTO.isValid    ( req.body );
+        console.log('req.body:', req.body)
+        await companysService.signUp( userData );
+
+        return res.status(201).json( {"message" : "Once you have completed your email authentication, your account will be activated."} );
+
+    } catch ( error ) {
+        console.log('[ERROR] - ', error)
+        return res.status(400).json( { "message" : "Bad Request" } );
+    }
+}
