bdce04d4bcbb37c8718a01b45987e66ace848a22
ltj <xowjd8465@gmail.com>
2025-05-22 10:51:30 +0900

    ltj add / login 비지니스 로직 추가

--- companys/service/companys.service.js
+++ companys/service/companys.service.js
@@ -10,7 +10,7 @@ exports.signUp = async ( userData ) => {
       if (queryResult.rows.length > 0) throw new Error('user is duplicate');
   
       const signUpToken = companysLogic.makeSignUpToken(userData.email);
-      
+
       await Promise.all([
         companysLogic.insertUserData(userData, signUpToken),
         companysLogic.sendMailForSignUp(userData.email, signUpToken)
@@ -47,3 +47,35 @@ exports.signUp = async ( userData ) => {
 //         throw new Error(error.message)
 //     }
 // }
+
+exports.login = async( userId, password ) => {
+    try {
+        // DB에서 userId값이 있는지 조회
+        let queryResult = await db.query( query.companyLogin, [ userId ] );
+        queryResult     = queryResult.rows[0];
+
+        // 없다면 Error
+        if ( queryResult === undefined   ) throw new Error('user not found')
+        
+        if ( queryResult.role !== NORMAL ) throw new Error('user not found')
+
+        
+        // 비밀번호 검증
+        if ( queryResult.password === password ) {
+
+            // token 생성 동시 시작 
+            const [ accessToken, refreshToken ] = await Promise.all ( [ companysLogic.makeAccessToken(userId), companysLogic.makeRefreshToken(userId) ] );
+            const updateDate                    = new Date();
+
+            // db에 token 및 현재 시간 update
+            await db.query( query.companyLoginSuccess, [ accessToken, refreshToken, updateDate, userId ] );
+
+            // token return
+            return { "access_token" : accessToken , "refresh_token" : refreshToken };
+        }
+        else throw new Error('user not found')
+        
+    } catch ( error ) {
+        throw new Error('user not found')
+    }
+}
