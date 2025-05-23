c1a2fd5287c88edce77bd06ba38a6a228c8799f7
ltj <xowjd8465@gmail.com>
2025-05-23 09:47:50 +0900

    ltj add / 주석제거

--- companys/service/companys.service.js
+++ companys/service/companys.service.js
@@ -17,35 +17,11 @@ exports.signUp = async ( userData ) => {
       await companysLogic.insertUserData( userData, signUpToken );
       await companysLogic.sendMailForSignUp( userData.email, signUpToken );
       return { message: 'Signup successful. Please verify your email.' };
-  
+      
     } catch (error) {
       throw new Error( error.message );
     }
   };
-
-// exports.signUp = async ( userData ) => {
-
-//     try {
-//         // lst add / 비동기로 llm을 활용한 문서 요약 함수 실행
-       
-//         let queryResult = await db.query( query.checkIdDuplicate, [userData.userId] );
-//         queryResult     = queryResult.rows;
-//         if ( queryResult == [] ) throw new Error('user is duplicate');
-
-//         const signUpToken    = companysLogic.makeSignUpToken  ( userData.email );
-//         await companysLogic.insertUserData( userData, await signUpToken );        
-        
-        
-       
-//         await companysLogic.sendMailForSignUp( userData.email, signUpToken );
-        
-        
-//         throw new Error('sucess')
-        
-//     } catch (error) {
-//         throw new Error(error.message)
-//     }
-// }
 
 exports.login = async( userId, password ) => {
     try {
