6d22b27ae7826de0b2f5e2e751c920f95fea1668
lst <lstlove9804@gmail.com>
2025-06-01 06:01:35 +0900

    lst add / 미들웨어 router 경로 별 줄 맞춤 및 주석들 추가

--- app.js
+++ app.js
@@ -19,23 +19,34 @@ const skillsRouter   = require('@skills_router');
 const companysRouter = require('@companys_router');
 const cors         = require("cors");
 
+// Swagger설정 사용
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use( express.json() );
 app.use(cors({
-  origin: 'http://localhost:5173', // ⚠️ '*' 말고 정확한 주소
+  origin: 'http://localhost:5173', // '*' 말고 정확한 주소
   credentials: true                // 쿠키 허용
 }));
 
+// MiddleWare 설정
+// 조건문에 묶인 path는 Token 검증을 하지 않는 path
 app.use( ( req, res, next ) => {
     if ( 
-        req.path === '/auth/login' || req.path === '/auth/signup' || 
-        req.path === '/companys/login' || req.path === '/companys/signup' || 
-        req.path === '/companys/signup-verify' || req.path === '/auth/token-refresh'
+        // auth 관련 
+        req.path === '/auth/login'                        || req.path === '/auth/signup'         || 
+        req.path === '/auth/token-refresh'                || req.path === '/auth/reset-password' ||
+        req.path === '/auth/reset-password/token-verify'  || req.path === '/auth/new-password'   ||
+
+        // company 관련
+        req.path === '/companys/login'                    || req.path === '/companys/signup'     || 
+        req.path === '/companys/signup-verify' 
+
     ) return next();
 
     middleWare.verifyToken( req, res, next );
 })
 
+// API Router 설정
 app.use( '/skills',   skillsRouter   );
 app.use( '/users',    usersRouter    );
 app.use( '/auth',     authRouter     );
