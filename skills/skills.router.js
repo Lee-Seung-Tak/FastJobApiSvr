e202929ff50be826d9a17a0bbd56e445def62969
ltj <xowjd8465@gmail.com>
2025-06-05 14:07:27 +0900

    ltj add / 스웨거 추가

--- skills/skills.router.js
+++ skills/skills.router.js
@@ -4,7 +4,28 @@ const skillsController   = require('@skills_controller');
 
 
 
-
+/**
+ * @swagger
+ * /skills/list:
+ *   get:
+ *     summary: 전체 기술 목록 조회
+ *     tags: [Skills]
+ *     responses:
+ *       200:
+ *         description: 모든 기술 목록 반환
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 skills:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                   example: ["JavaScript", "Node.js", "React", "Python"]
+ *       500:
+ *         description: 서버 오류 - 기술 목록 조회 실패
+ */
 router.get('/list', skillsController.getAllSkills);
 module.exports = router;
 
