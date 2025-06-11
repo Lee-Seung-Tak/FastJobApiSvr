f449ed702c743d3029c9b32ce4a4ca6e94f82f76
lst <lstlove9804@gmail.com>
2025-06-11 23:53:08 +0900

    lst add / to-do list 추가

--- ai_analyze/ai.analyze.js
+++ ai_analyze/ai.analyze.js
@@ -35,4 +35,11 @@ exports.aiAnalyzeCarrerDesc = async ( userId, fileUrl ) => {
 }
 
 
+// TO-DO lts
+/*
+    1. 채용공고에 지원한 지원자들의 이력서, 경력기술서, 자기소개서를 바탕으로 어떤 사용자가 가장 지원한 공고에 적합한지 회사측에
+    추천해주는 함수를 구현하세요.
 
+    2. Rag를 활용하여 구축하세요.
+ */
+// end
