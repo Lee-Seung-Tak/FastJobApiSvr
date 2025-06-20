6eb20f1d7c8dc12cf5f9883c69c3c6e0bf5008c4
ltj <xowjd8465@gmail.com>
2025-06-20 19:17:46 +0900

    ltj add / postId => jobPostingId, update => insert로 변경

--- users/service/users.service.js
+++ users/service/users.service.js
@@ -118,15 +118,15 @@ exports.myJobApplications = async ( userId ) => {
   return getMyJobApplications.length ? getMyJobApplications : null;
 };
 
-exports.submitApplication = async ( userId, postPk ) => {
+exports.submitApplication = async ( userId, jobPostingId ) => {
   try {
     const userPk            = ( await db.query( query.getUserPk, [userId] ) ).rows[0].id;
-    const duplicateResult   = await db.query( query.duplicateApplication, [userPk, postPk] )
+    const duplicateResult   = await db.query( query.duplicateApplication, [userPk, jobPostingId] )
     
     if (duplicateResult.rows.length > 0) {
       throw new Error ("You have already applied for this job")
     }
-    const queryResult       = await db.query( query.updateUserApplications, [userPk, postPk] );
+    const queryResult       = await db.query( query.insertUserApplications, [userPk, jobPostingId] );
     const deletedId         = queryResult.rows[0].id;
     await db.query( query.insertJobApplication, [ userPk, deletedId ] );
   
@@ -136,15 +136,15 @@ exports.submitApplication = async ( userId, postPk ) => {
   }
 };
 
-exports.deleteApplication = async ( userId, postId ) => {
+exports.deleteApplication = async ( userId, jobPostingId ) => {
   try {
     const userPk                   = ( await db.query( query.getUserPk, [userId] ) ).rows[0].id;
-    const deleteJobApplication     = await db.query( query.deleteJobApplication, [userPk, postId]);
+    const deleteJobApplication     = await db.query( query.deleteJobApplication, [userPk, jobPostingId]);
     if ( deleteJobApplication.rowCount === 0 ) {
       throw new Error('No application record found to delete.');
     }
-    await db.query( query.deleteApplicationById, [postId]);
-  } catch ( error ){
+    await db.query( query.deleteApplicationById, [jobPostingId]);
+  } catch ( error ) {
     console.log( error );
     throw error;
   }
