5e9435be6cfbebcdb95eb80c779465b9c05d48ec
chaeyeon <codus1733@gmail.com>
2025-06-22 21:15:48 +0900

    cy add / 채용공고 수정 기능추가 + 열 맞추기

--- database/query.js
+++ database/query.js
@@ -337,26 +337,19 @@ export const uploadRecruitJob = `INSERT INTO company.recruit_post (
 ;
 
 export const getId = `SELECT id FROM company.company_account
-WHERE company_id = $1`
+  WHERE company_id = $1`
 ;
 
 //채용공고 삭제
 export const findRecruitJob = `SELECT * FROM company.recruit_post
-WHERE id = $1`
+  WHERE id = $1`
 ;
 
 export const deleteRecruitJob = `DELETE FROM company.recruit_post
-WHERE id = $1`
+  WHERE id = $1`
 ;
-  
-// export const updateRecruitJob = `
-//   UPDATE company.recruit_post
-//   SET
-//     title = $2,
-//     description = $3,
-//     category = $4,
-//     deadline = $5,
-//     created_at = NOW(),
-//     is_active = $6
-//   WHERE id = $1;
-// `;
+
+export const updateRecruitJob = ( fields ) => `UPDATE company.recruit_post
+  SET ${fields.join(', ')}
+  WHERE id = $1`
+;
