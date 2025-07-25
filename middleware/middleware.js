7db0d49dcff00bbeb4685029cf5093bc9548226d
ltj <xowjd8465@gmail.com>
2025-07-25 09:48:05 +0900

    ltj add / 미들웨어에서 companyId 디코딩 및 요청에 추가

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -14,9 +14,10 @@ exports.verifyToken = (req, res, next) => {
         return res.status(401).json({ message: 'Token not found' });
     }
 
-    try {
-        const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+    try {        
+        const decoded = jwt.verify(token, process.env.ACCESS_SECRET);        
         req.userId = decoded.userId; // 디코딩된 사용자 정보 저장
+        req.companyId = decoded.companyId;
         next();
     } catch (err) {
         return res.status(403).json({ message: 'Invalid or expired token' });
